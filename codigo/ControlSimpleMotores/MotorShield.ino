45e8df1ba9e35006e3667fe16dda7ace82b0913c
Javacasm <javacasm@gmail.com>
2018-11-29 12:45:25 +0100

    add

---
+++ codigo/ControlSimpleMotores/MotorShield.ino
@@ -0,0 +1,16 @@
+#include "MotorShield.h"
+
+void setup_reles(){
+    pinMode(RELE_1, OUTPUT); 
+    pinMode(RELE_2, OUTPUT); 
+}
+
+
+void apagaRele(int pinRele){
+    digitalWrite(pinRele, LOW);
+}
+
+void enciendeRele(int pinRele){
+    digitalWrite(pinRele, HIGH);
+}
+
